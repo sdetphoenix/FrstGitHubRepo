96adf6085180b9a913d83da81b168a77e14c1bef
sdetphoenix <phoenixsdet@gmail.com>
2022-01-04 21:41:08 -0500

    test test

--- src/BranchExample.java
+++ src/BranchExample.java
@@ -3,7 +3,12 @@ public class BranchExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	
 
+	}
+	public void testA() {
+		System.out.println("this is example");
 	}
 
 }
