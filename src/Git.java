823fb1dccd7dcb4b9626e04f578705225f83baae
sdetphoenix <97071667+sdetphoenix@users.noreply.github.com>
2022-01-04 19:36:48 -0500

    Update Git.java
    This is changes in Repo

--- src/Git.java
+++ src/Git.java
@@ -3,7 +3,13 @@ public class Git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
 
+	}
+	
+	public void methodA(){
+		System.out.println("this is changes from upstream, upstream means in remote repository");
+		
 	}
 
 }
