27fbb5f10ccbd06b51cbc39a0099c571d32b8ef9
sdetphoenix <97071667+sdetphoenix@users.noreply.github.com>
2022-01-04 19:52:35 -0500

    Update Git.java
    this is for practice

--- src/Git.java
+++ src/Git.java
@@ -11,5 +11,9 @@ public class Git {
 		System.out.println("this is changes from upstream, upstream means in remote repository");
 		
 	}
+	
+	public void methodB(){
+		// This is second mehtod for practice purpose 
+	}
 
 }
